066277eae327449fdb6ab7e433acaec691835629
Koutoulakis <manos.koutoulak@gmail.com>
2021-10-26 16:42:46 +0200

    undistort frame

--- scripts/webcam_sub.py
+++ scripts/webcam_sub.py
@@ -66,12 +66,23 @@ def callback(data):
   # the markers
   aruco_dict_type = cv2.aruco.DICT_ARUCO_ORIGINAL
 
-  k = np.array([[364.03943, 0., 626.78017],
+  # TODO: get those matrix automatically and not hardcoded
+  camera_matrix = np.array([[364.03943, 0., 626.78017],
             [0., 363.3541, 513.60797],
             [0., 0. ,1.]])
-  d = np.array([-0.162588, 0.016767, -0.001129, 0.000068, 0.000000])
+  distortion_matrix = np.array([-0.162588, 0.016767, -0.001129, 0.000068, 0.000000])
 
-  output = pose_esitmation(current_frame, aruco_dict_type, k, d)
+  h, w = current_frame.shape[:2]
+
+  # Undistort the frame
+  new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, distortion_matrix, (w,h), 1, (w,h))
+  undistort_frame = cv2.undistort(current_frame, camera_matrix, distortion_matrix, None, new_camera_matrix)
+
+  # Crop the frame
+  x, y, w, h = roi
+  undistort_frame = undistort_frame[y:y+h, x:x+w]
+
+  output = pose_esitmation(undistort_frame, aruco_dict_type, camera_matrix, distortion_matrix)
 
   cv2.imshow('Estimated Pose', output)
 
